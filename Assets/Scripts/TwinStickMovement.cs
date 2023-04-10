ac876abd6ff81bbeffd912fb4e26e353fd36ff20
Guillaume.P <guillaume.pisseloup@gmail.com>
2023-04-10 17:17:53 +0200

    improving movement blend tree

--- Assets/Scripts/TwinStickMovement.cs
+++ Assets/Scripts/TwinStickMovement.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 using UnityEngine.Windows;
@@ -23,13 +24,13 @@ public class TwinStickMovement : MonoBehaviour
     private Vector3 playerVelocity;
 
     private PlayerControls playerControls;
-    private PlayerInput playerInput;
+    //private PlayerInput playerInput;
 
     //
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
-        playerInput = GetComponent<PlayerInput>();
+        //playerInput = GetComponent<PlayerInput>();
         animator = GetComponent<Animator>();
         playerControls = new PlayerControls();
     }
@@ -51,8 +52,6 @@ public class TwinStickMovement : MonoBehaviour
         HandleMovement();
         HandleRotation();
 
-        Debug.Log("movement: " + movement);
-
         Vector3 moveDirection = new Vector3(movement.x, 0, movement.y);
 
         if (moveDirection.magnitude > 0.01f)
@@ -65,15 +64,6 @@ public class TwinStickMovement : MonoBehaviour
 
             animator.SetFloat("InputX", Mathf.Lerp(animator.GetFloat("InputX"), targetInputX, Time.deltaTime * smoothness));
             animator.SetFloat("InputY", Mathf.Lerp(animator.GetFloat("InputY"), targetInputY, Time.deltaTime * smoothness));
-
-
-            //float angle = Vector3.SignedAngle(transform.forward, moveDirection.normalized, Vector3.up);
-            //float inputX = Mathf.Sin(angle * Mathf.Deg2Rad);
-            //float inputY = Mathf.Cos(angle * Mathf.Deg2Rad);
-
-
-            //animator.SetFloat("InputX", inputX);
-            //animator.SetFloat("InputY", inputY);
         }
         else
         {
@@ -81,16 +71,14 @@ public class TwinStickMovement : MonoBehaviour
             animator.SetFloat("InputX", 0);
             animator.SetFloat("InputY", 0);
         }
-
-        //animator.SetFloat("InputX", movement.x);
-        //animator.SetFloat("InputY", movement.y);
-
     }
+
     private void HandleInput()
     {
         movement = playerControls.Controls.Movements.ReadValue<Vector2>();
         aim = playerControls.Controls.Aim.ReadValue<Vector2>();
     }
+
     private void HandleMovement()
     {
         Vector3 move = new Vector3(movement.x, 0, movement.y);
@@ -109,7 +97,10 @@ public class TwinStickMovement : MonoBehaviour
         if (groundPlane.Raycast(ray, out rayDistance))
         {
             Vector3 point = ray.GetPoint(rayDistance);
-            LookAt(point);
+            if (new Vector3(movement.x, 0, movement.y).magnitude > 0.01f)
+            {
+                LookAt(point);
+            }
         }
     }
 
