9931ba43e05eec25d653e698a64fb95d8b3760a8
Guillaume.P <guillaume.pisseloup@gmail.com>
2024-03-19 16:17:57 +0100

    adjustments to player interaction with interactables and bonus setup

--- Assets/Scripts/Bonus.cs
+++ Assets/Scripts/Bonus.cs
@@ -1,40 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class Bonus : MonoBehaviour
 {
-    [SerializeField] private KeyCode interactKey = KeyCode.E;
     [SerializeField] private Perk perk;
-    private bool playerInRange = false;
 
-    private void Update()
+    public void GetBonus()
     {
-        // Check if the player presses the interactKey
-        if (Input.GetKeyDown(interactKey) && playerInRange)
-        {
-            // Call the SelectPerk method from PerksManager
-            PerksManager.Instance.SelectPerk(perk);
-        }
+        // Call the SelectPerk method from PerksManager
+        PerksManager.Instance.SelectPerk(perk);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        // Check if the collider belongs to the player
-        if (other.CompareTag("Player"))
-        {
-            // Set the flag to true when the player enters the collider
-            playerInRange = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        // Check if the collider belongs to the player
-        if (other.CompareTag("Player"))
-        {
-            // Set the flag to false when the player exits the collider
-            playerInRange = false;
-        }
-    }
 }
